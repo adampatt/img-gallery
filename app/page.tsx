975f8c5816358f853b344932ff26867b6688ba88
Adam <adampattison08@gmail.com>
2024-04-16 13:41:57 +0200

    feat: use auth to display sign in on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,44 @@
 import Image from 'next/image';
 import { db } from './server/db';
+import { SignedIn, SignedOut } from '@clerk/nextjs';
 
-export default async function Home() {
+async function Images() {
   const images = await db.query.images.findMany({
     // Id is incremented each time a new image is added so we are displaying newest first by ordering by id in descending order
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div>
+      {images.map((image) => (
+        <div
+          key={image.id}
+          className="flex flex-col flex-wrap gap-4"
+        >
+          <Image
+            src={image.url}
+            alt={image.url}
+            width={400}
+            height={400}
+          />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function Home() {
+  return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-row flex-wrap gap-4">
-        {images.map((image) => (
-          <div
-            key={image.id}
-            className="w-48 gap-4 flex flex-col"
-          >
-            <Image
-              src={image.url}
-              alt={image.url}
-              width={400}
-              height={400}
-            />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div>
+          <h1 className="text-4xl font-bold">Welcome to the image gallery please sign in above</h1>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
