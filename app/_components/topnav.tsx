824cab175cc948f52f97d3787e8000d214637ea5
Adam <adampattison08@gmail.com>
2024-04-16 14:15:49 +0200

    feat: add image upload button and page refresh

--- app/_components/topnav.tsx
+++ app/_components/topnav.tsx
@@ -1,15 +1,26 @@
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+'use client';
 
+import { UploadButton } from '@/utils/uploadthing';
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
 export function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full justify-between items-center p-4 text-xl font-semibold border-b">
       <div> Gallery </div>
-      <div>
+      <div className="flex flex-row">
         {/* Only rendered if you are signed out */}
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              router.refresh();
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
 const inter = Inter({ subsets: ['latin'] });
 import { TopNav } from './_components/topnav';
+import '@uploadthing/react/styles.css';
+import { NextSSRPlugin } from '@uploadthing/react/next-ssr-plugin';
+import { extractRouterConfig } from 'uploadthing/server';
+import { ourFileRouter } from './api/uploadthing/core';
 
 export const metadata: Metadata = {
   title: 'Image gallery',
@@ -18,6 +22,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <body className={inter.className}>
           <TopNav />
           {children}
