9106ad5fd2ade130b4b02b361cf4012898bb4403
Adam <adampattison08@gmail.com>
2024-04-18 09:55:47 +0200

    style: add styling to fullpage view component

--- app/components/full-image-page.tsx
+++ app/components/full-image-page.tsx
@@ -5,10 +5,17 @@ export default async function FullPageImageView(props: { photoId: number }) {
   const { photoId } = props;
   const image = await getImage(photoId);
   return (
-    <img
-      className="w-96"
-      src={image.url}
-      alt={image.name}
-    />
+    <div className="flex w-full h-full min-w-0">
+      <div className="flex-shrink flex justify-center items-center">
+        <img
+          className="object-contain"
+          src={image.url}
+          alt={image.name}
+        />
+      </div>
+      <div className="w-48 flex flex-col flex-shrink-0 border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
   );
 }
